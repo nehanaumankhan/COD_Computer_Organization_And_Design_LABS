0947a57603f6a282cd0d0f89514aecbcc5043b01
nehanaumankhan <nehanaumankhan@gmail.com>
2024-05-12 12:24:59 +0500

    update

--- LaB6/COD LAB 6 EXERCISE 1.asm
+++ LaB6/COD LAB 6 EXERCISE 1.asm
@@ -1,48 +1,60 @@
 .data
-
-    msg1: .asciiz "THIS PROGRAM PRINTS THE SUM OF N INTEGERS\n\nENTER THE VALUE OF N: "
-    msg2: .asciiz "SUM TILL N (1+2+3+...+n) = "
-    error_msg: .asciiz "\nERROR: PLEASE ENTER A POSITIVE INTEGER!\n\n"
+    msg1: .asciiz "THIS PROGRAM PRINTS THE SUM OF N INTEGERS\n\nENTER THE VALUE OF N: "  # Prompt for user to enter the value of N
+    msg2: .asciiz "SUM TILL N (1+2+3+...+n) = "  # Message for displaying the sum till N
+    error_msg1: .asciiz "\nERROR: PLEASE ENTER A POSITIVE INTEGER!\n\n"  # Error message for negative integer input
 
 .text
 .globl main
 .ent main
 
 main:
+    # Display prompt to enter the value of N
+    li $v0, 4                   
+    la $a0, msg1               
+    syscall                     
 
-    li $v0, 4                   # preparing for string display
-    la $a0, msg1
-    syscall                     # msg1 will be displayed on console
+    # Get integer input for N
+    li $v0, 5                  
+    syscall                     
+    move $s0, $v0               
 
-    li $v0, 5                   # preparing for integer input
-    syscall                     # integer n was put in $v0
-    move $s0, $v0               # integer n is moved to $s0
-
-    slti $t0, $s0, 0			# $t0 = ($s0 < 0) ? 1 : 0
+    # Check if N is negative
+    slti $t0, $s0, 0			
     bne $t0, $0, ERROR
 
-    addi $t0, $0, 1             # index variable of the loop -> t0 = 1
-    addi $t1, $0, 0             # t1 stores the result of addition in each iteration
-    addi $s0, $s0, 1
-    LOOP:
-        beq $t0, $s0, EXIT      # When t0 == n -> the loop will terminate
-        add $t1, $t1, $t0 
-        addi $t0, $t0, 1
-        j LOOP
-    EXIT:
-        li $v0, 4                   # preparing for string display
-        la $a0, msg2
-        syscall                     # msg1 will be displayed on console
+    # Initialize loop variables
+    addi $t0, $0, 1             # Index variable for the loop (t0 = 1)
+    addi $t1, $0, 0             # Variable to store the sum of integers (t1)
 
-        li $v0, 1           # preparing for integer output
-        move $a0, $t1       # result is moved to $a0
-        syscall             # result is displayed on console
+    # Increment N by 1 for the loop termination condition
+    addi $s0, $s0, 1           
+
+    # Loop to calculate the sum of integers from 1 to N
+    LOOP:
+        beq $t0, $s0, EXIT      # Exit the loop when t0 == N
+        add $t1, $t1, $t0       # Add t0 to the sum
+        addi $t0, $t0, 1        # Increment t0 by 1 for the next iteration
+        j LOOP                  # Jump back to LOOP
+
+    # Display the sum of integers
+    EXIT:
+        li $v0, 4               
+        la $a0, msg2            
+        syscall                 
+
+        li $v0, 1               
+        move $a0, $t1           
+        syscall                 
         j END
+
+    # Display error message for negative input
     ERROR:
-        li $v0, 4                   # preparing for string display
-        la $a0, error_msg
-        syscall                     # msg1 will be displayed on console
-        j main
+        li $v0, 4               
+        la $a0, error_msg      
+        syscall                 
+        j main                  
+
+    # End of the program
     END:
-        jr $ra
+        jr $ra                  
         .end main
