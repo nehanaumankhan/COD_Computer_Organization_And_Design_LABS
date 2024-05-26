c9908f89e05dc84d0383fd3d840f7e507c948bfc
nehanaumankhan <nehanaumankhan@gmail.com>
2024-05-26 12:05:16 +0500

    Exceptions Added

--- LAB 7/COD LAB 7 EXERCISE 2.asm
+++ LAB 7/COD LAB 7 EXERCISE 2.asm
@@ -2,6 +2,7 @@
     msg1: .asciiz "THIS PROGRAM CALCULATES (NUMBER ^ POWER)\nENTER NUMBER: "
     msg2: .asciiz "ENTER POWER: "
     msg3: .asciiz "RESULT: "
+    ERROR: .asciiz "📌ERROR: PLEASE ENTER POSITIVE NUMBER!\n"
 
 .text
 .globl main
@@ -23,8 +24,16 @@ main:   li $v0, 4           # Print msg1
         syscall
         move $a2, $v0       # POWER is moved in $a2
 
+        slti $t0, $a2, -1
+        beq $t0, $0, CALCULATE_POWER
+        
+        li $v0, 4           # Print ERROR
+        la $a0, ERROR
+        syscall
+        j main          
+
         # We haven't used any temporary registers in main -> We do not have to push any register before function call
-        jal power_calculator
+        CALCULATE_POWER:    jal power_calculator
 
         li $v0, 4           # Print msg3
         la $a0, msg3
@@ -39,7 +48,13 @@ main:   li $v0, 4           # Print msg1
         jr $ra
         .end main
 
-        power_calculator:   addi $sp, $sp, -8   # Making room for 4 registers
+        power_calculator:   
+                            bne $a2, $0, CONTINUE
+                            addi $v1, $0, 1
+                            j END
+                            
+                            CONTINUE:
+                            addi $sp, $sp, -8   # Making room for 4 registers
                             sw $s0, 4($sp)      # Push $s0
                             sw $s1, 0($sp)      # Push $s1
 
@@ -51,7 +66,8 @@ main:   li $v0, 4           # Print msg1
                                     bne $s1, $a2, LOOP  # If $s1 != $a2 -> Jump To Loop
 
                             move $v1, $s0       # Move $s0 in $v1
-
+                            
+                            END:
                             lw $s1, 0($sp)      # Pop $s1
                             lw $s0, 4($sp)      # Pop $s0
                             addi $sp, $sp, 8    # Adust SP        
